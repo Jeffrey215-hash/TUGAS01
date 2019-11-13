b5db089560e9a6fff24e65e2a681a5624b88522d
jeffreylouise <jeffreylouise19@gmail.com>
2019-11-13 18:09:34 +0700

    tambah contact

--- gen/com/example/tugas01/R.java
+++ gen/com/example/tugas01/R.java
@@ -10,33 +10,63 @@ package com.example.tugas01;
 public final class R {
     public static final class attr {
     }
+    public static final class dimen {
+        /**  Default screen margins, per the Android Design guidelines. 
+
+         Customize dimensions originally defined in res/values/dimens.xml (such as
+         screen margins) for sw720dp devices (e.g. 10" tablets) in landscape here.
+    
+         */
+        public static final int activity_horizontal_margin=0x7f040000;
+        public static final int activity_vertical_margin=0x7f040001;
+    }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int email=0x7f06000b;
-        public static final int list=0x7f060009;
-        public static final int name=0x7f06000a;
-        public static final int phone=0x7f06000c;
-        public static final int tvaddressl=0x7f060001;
-        public static final int tvaddressv=0x7f060002;
-        public static final int tvdobl=0x7f060007;
-        public static final int tvdobv=0x7f060008;
-        public static final int tvemaill=0x7f060003;
-        public static final int tvemailv=0x7f060004;
-        public static final int tvname=0x7f060000;
-        public static final int tvphonel=0x7f060005;
-        public static final int tvphonev=0x7f060006;
+        public static final int action_settings=0x7f080018;
+        public static final int btsimpan=0x7f080014;
+        public static final int bttambah=0x7f080009;
+        public static final int email=0x7f080016;
+        public static final int etaddress=0x7f08000d;
+        public static final int etdob=0x7f080013;
+        public static final int etemail=0x7f08000f;
+        public static final int etname=0x7f08000b;
+        public static final int etphone=0x7f080011;
+        public static final int list=0x7f08000a;
+        public static final int name=0x7f080015;
+        public static final int phone=0x7f080017;
+        public static final int tvaddress=0x7f08000c;
+        public static final int tvaddressl=0x7f080001;
+        public static final int tvaddressv=0x7f080002;
+        public static final int tvdob=0x7f080012;
+        public static final int tvdobl=0x7f080007;
+        public static final int tvdobv=0x7f080008;
+        public static final int tvemail=0x7f08000e;
+        public static final int tvemaill=0x7f080003;
+        public static final int tvemailv=0x7f080004;
+        public static final int tvname=0x7f080000;
+        public static final int tvphone=0x7f080010;
+        public static final int tvphonel=0x7f080005;
+        public static final int tvphonev=0x7f080006;
     }
     public static final class layout {
         public static final int activity_contact_detail=0x7f030000;
         public static final int activity_main=0x7f030001;
-        public static final int list_item=0x7f030002;
+        public static final int activity_tambah_contact=0x7f030002;
+        public static final int list_item=0x7f030003;
+    }
+    public static final class menu {
+        public static final int contact_add=0x7f070000;
+        public static final int tambah_contact=0x7f070001;
     }
     public static final class string {
-        public static final int app_name=0x7f040000;
-        public static final int hello_world=0x7f040001;
-        public static final int title_activity_contact_detail=0x7f040002;
+        public static final int action_settings=0x7f050004;
+        public static final int app_name=0x7f050000;
+        public static final int hello_world=0x7f050001;
+        public static final int title_activity_contact_add=0x7f050003;
+        public static final int title_activity_contact_detail=0x7f050002;
+        public static final int title_activity_tambah_contact=0x7f050005;
     }
     public static final class style {
         /** 
@@ -60,10 +90,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f050000;
+        public static final int AppBaseTheme=0x7f060000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f050001;
+        public static final int AppTheme=0x7f060001;
     }
 }
